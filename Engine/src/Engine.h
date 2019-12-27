4a1a05e1db493966c0c3acad8f381907d398a15a
LimarDeWills <53938159+LimarDeWills@users.noreply.github.com>
2019-12-27 10:42:39 +0300

    Setup basic applications and entery point

---
+++ Engine/src/Engine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//For use by Engine applications
+#include "Engine/Application.h"
+
+//------Entry point----------------
+#include "Engine/EntryPoint.h"
+//---------------------------------
